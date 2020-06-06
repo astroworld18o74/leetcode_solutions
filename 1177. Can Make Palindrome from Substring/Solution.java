c6542d53f701a22ba49a169dcd110720f3c5485c
chandelsahil04 <46382675+chandelsahil04@users.noreply.github.com>
2020-06-07 01:10:36 +0530

    Create Solution.java
    https://leetcode.com/problems/can-make-palindrome-from-substring/

---
+++ 1177. Can Make Palindrome from Substring/Solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<Boolean> canMakePaliQueries(String s, int[][] queries) {
+        int str_len = s.length();
+    int j = 0;
+    List<Boolean> results = new ArrayList<>();
+    int[] masks = new int[str_len + 1];
+    
+    int mask = 0;
+    for(int i = 0 ; i < str_len; i++){
+        mask ^= (1 << (s.charAt(i) - 'a'));
+        masks[++j] = mask;
+    }
+
+    for(int [] query : queries){
+        if(query[2] >= 13){
+            results.add(true);
+        }else{
+            results.add(Integer.bitCount(masks[query[1] + 1] ^ masks[query[0]]) /2 <= query[2]);
+        }
+    }
+
+    return results;
+    }
+}
