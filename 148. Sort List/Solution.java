0c5655487320bcdca787ab8044d06d1c8c269845
chandelsahil04 <46382675+chandelsahil04@users.noreply.github.com>
2020-06-07 23:13:17 +0530

    https://leetcode.com/problems/sort-list/
    https://leetcode.com/problems/sort-list/

---
+++ 148. Sort List/Solution.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if(head==null || head.next==null)return head;
+        ListNode slow=head,fast=head;
+        while(fast.next!=null && fast.next.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        ListNode middle=slow;
+        ListNode middlenext=middle.next;
+        middle.next=null;
+        ListNode left=sortList(head);
+        ListNode right=sortList(middlenext);
+        ListNode result=sortedmerge(left,right);
+        return result;
+    }
+    public ListNode sortedmerge(ListNode l1, ListNode l2) {
+    ListNode l = new ListNode(0), p = l;
+    
+    while (l1 != null && l2 != null) {
+      if (l1.val < l2.val) {
+        p.next = l1;
+        l1 = l1.next;
+      } else {
+        p.next = l2;
+        l2 = l2.next;
+      }
+      p = p.next;
+    }
+    
+    if (l1 != null)
+      p.next = l1;
+    
+    if (l2 != null)
+      p.next = l2;
+    
+    return l.next;
+  }
+}
