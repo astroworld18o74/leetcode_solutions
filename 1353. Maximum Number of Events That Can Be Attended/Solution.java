24fecddde60f74a93518b2f153a7928755d0f574
chandelsahil04 <46382675+chandelsahil04@users.noreply.github.com>
2020-06-09 03:33:19 +0530

    https://leetcode.com/problems/maximum-number-of-events-that-can-be-attended/
    https://leetcode.com/problems/maximum-number-of-events-that-can-be-attended/

---
+++ 1353. Maximum Number of Events That Can Be Attended/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+     public int maxEvents(int[][] A) {
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+        Arrays.sort(A, (a, b) -> Integer.compare(a[0], b[0]));
+        int i = 0, res = 0, n = A.length;
+        for (int d = 1; d <= 100000; ++d) {
+            while (!pq.isEmpty() && pq.peek() < d)
+                pq.poll();
+            while (i < n && A[i][0] == d)
+                pq.offer(A[i++][1]);
+            if (!pq.isEmpty()) {
+                pq.poll();
+                ++res;
+            }
+            if(pq.isEmpty() && i>=n)
+                break;
+        }
+        return res;
+    }
+}
