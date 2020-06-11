dcd0d14c7a59fb44ddede6d85c614cdd9777b36e
chandelsahil04 <46382675+chandelsahil04@users.noreply.github.com>
2020-06-11 16:36:26 +0530

    https://leetcode.com/problems/remove-palindromic-subsequences/
    https://leetcode.com/problems/remove-palindromic-subsequences/

---
+++ 1332. Remove Palindromic Subsequences/Solution.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int removePalindromeSub(String s) {
+        if(s.isEmpty())return 0;
+        if(s.equals(new StringBuilder(s).reverse().toString()))return 1;
+        return 2;
+    }
+}
